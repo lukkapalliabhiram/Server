b6a0421d28f7bbf9e7f1d413799993dbaf7ddbc2
lukkapalliabhiram <78559926+lukkapalliabhiram@users.noreply.github.com>
2023-04-07 00:07:55 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@ module.exports = {
   GOOGLE_CLIENT_ID: '701163878008-v9r4hj13l4ra32qsf71p1n0821n7iu70.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: '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',
   URL : 'https://backend-4gbl.onrender.com',
-  GMAIL_USER: 'hanabiyuga2023@gmail.com',
-  GMAIL_PASS: 'ojfdlqwcsrjleefu',
+  GMAIL_USER: 'kukkapalliabhiram@gmail.com',
+  GMAIL_PASS: 'Abcdbetabd@77',
   client_URL: 'https://hanabi-hyuga.onrender.com',
 }; 
