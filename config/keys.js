2ae62d0f18c610d578b5930a16092261aee60f18
lukkapalliabhiram <78559926+lukkapalliabhiram@users.noreply.github.com>
2023-04-06 23:28:39 -0400

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ module.exports = {
   secretOrKey: "secret",
   GOOGLE_CLIENT_ID: '701163878008-v9r4hj13l4ra32qsf71p1n0821n7iu70.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: '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',
-  URL : 'http://localhost:3500',
+  URL : 'https://backend-4gbl.onrender.com',
   GMAIL_USER: 'hanabiyuga2023@gmail.com',
   GMAIL_PASS: 'ojfdlqwcsrjleefu',
   client_URL: 'https://hanabi-hyuga.onrender.com',
